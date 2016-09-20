6302eb8a51b4d9e5f30f65b30b8d8b3716d74dd8
Lipu Hossain <lipuhossain@mds-MacBook-Pro.local>
2016-09-21 02:35:32 +0600

    added tableView

---
+++ MainTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTableViewController.swift
+//  PandorasBox
+//
+//  Created by Lipu Hossain on 9/21/16.
+//  Copyright © 2016 lipple. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+    	super.viewDidLoad()
+
+    }
+
+}
